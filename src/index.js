71bd25d313ae8f428d454f1934aca939453cfef2
parul-mishra <parul.mishra@experitest.com>
2020-09-10 00:02:47 +0530

    add all files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './screens/login/Login';
+import Login from './screens/login/Login';
+
+
+ReactDOM.render(
+    <div>
+        <Login />
+    </div>, 
+    document.getElementById('root')
+);
+// ReactDOM.render(<Login />, document.getElementById('root'));
